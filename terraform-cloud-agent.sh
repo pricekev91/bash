eafb32a1b793ad3387175c302b2111e02a1e48e7
pricekev91 <55143215+pricekev91@users.noreply.github.com>
2023-11-29 11:26:09 -0500

    Update terraform-cloud-agent.sh

--- terraform-cloud-agent.sh
+++ terraform-cloud-agent.sh
@@ -6,18 +6,6 @@ version="1.14.1"
 # Download the agent
 wget https://releases.hashicorp.com/tfc-agent/${version}/tfc-agent_${version}_linux_amd64.zip
 
-# Download the checksum
-wget https://releases.hashicorp.com/tfc-agent/${version}/tfc-agent_${version}_SHA256SUMS
-
-# Download the checksum signature
-wget https://releases.hashicorp.com/tfc-agent/${version}/tfc-agent_${version}_SHA256SUMS.sig
-
-# Verify the signature file (you'll need to import HashiCorp's public key if you haven't already)
-gpg --verify tfc-agent_${version}_SHA256SUMS.sig tfc-agent_${version}_SHA256SUMS
-
-# Verify the SHA256 checksum of the zip file
-sha256sum -c tfc-agent_${version}_SHA256SUMS 2>&1 | grep OK
-
 # Unzip the agent
 unzip tfc-agent_${version}_linux_amd64.zip
 
