96083431c3ec943eb9e499c965762831c565b0ba
pricekev91 <55143215+pricekev91@users.noreply.github.com>
2023-11-29 12:02:16 -0500

    Update terraform-cloud-agent.sh

--- terraform-cloud-agent.sh
+++ terraform-cloud-agent.sh
@@ -16,5 +16,23 @@ chmod +x tfc-agent
 echo "Please enter your Terraform token:"
 read token
 
-# Run the agent
-./tfc-agent -token=$token
+# Define the file path
+FILE="/etc/systemd/system/tfc-agent.service"
+
+# Use sudo to create the file and write the content
+sudo bash -c "cat > $FILE" << EOF
+[Unit]
+Description=Terraform Cloud Agent
+After=network.target
+
+[Service]
+ExecStart=/path/to/tfc-agent -token=\$token
+
+[Install]
+WantedBy=multi-user.target
+EOF
+# Enable the service to start on boot
+sudo systemctl enable tfc-agent
+
+# Start the service
+sudo systemctl start tfc-agent
