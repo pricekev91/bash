991fdb7eae0031ee6f455eb76c98038c63e687a7
pricekev91 <55143215+pricekev91@users.noreply.github.com>
2023-12-06 11:58:40 -0500

    Update upost.sh

--- upost.sh
+++ upost.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-snap install btop
-apt-get install git
-apt-get install unzip
+sudo apt-get update # Fetches the list of available updates
+sudo apt-get upgrade # Installs some updates; does not remove packages
+sudo apt-get full-upgrade # Installs updates; may also remove some packages, if needed
+sudo apt-get autoremove # Removes any old packages that are no longer needed
+sudo snap install btop #installs btop monitoring tool
+sudo apt-get install git #duh git
+sudo apt-get install unzip #unzip which is needed for tfc agent install.
