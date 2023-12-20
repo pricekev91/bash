fda3d960a80e0f3fdbf895857cf381bff964ed81
pricekev91 <55143215+pricekev91@users.noreply.github.com>
2023-12-20 09:31:54 -0500

    Update upost.sh

--- upost.sh
+++ upost.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get update # Fetches the list of available updates
 sudo apt-get upgrade # Installs some updates; does not remove packages
-sudo apt-get full-upgrade # Installs updates; may also remove some packages, if needed
 sudo apt-get autoremove # Removes any old packages that are no longer needed
 sudo snap install btop #installs btop monitoring tool
 sudo apt-get install git -y #duh git
