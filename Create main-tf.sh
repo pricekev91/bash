c9417504e607479c8eb296a98713aad02f280081
pricekev91 <55143215+pricekev91@users.noreply.github.com>
2023-12-21 11:05:58 -0500

    Update Create main-tf.sh

--- Create main-tf.sh
+++ Create main-tf.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Define the directory where you want to save the file
+DIR="/home/root/terraform-projects/Alpaca AI Assistant Proj"
+
+# Create the directory if it doesn't exist
+mkdir -p "$DIR"
+
+# Define the file path
+FILE="$DIR/main.tf"
+
+# Write the Terraform configuration to the file
+cat > "$FILE" << EOF
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
@@ -8,3 +21,7 @@ terraform {
     }
   }
 }
+EOF
+
+# Print a message to indicate success
+echo "Terraform configuration has been written to $FILE"
